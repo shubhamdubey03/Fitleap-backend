354005f0ddb061dad429ad2de0ec5d30bc64741e
shubhamdubey03 <shubham.dubeyargos@gmail.com>
2026-03-22 10:49:16 +0530

    changes done

--- config/mailer.js
+++ config/mailer.js
@@ -1,13 +1,20 @@
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    secure: false, // true for 465
-    auth: {
-        user: process.env.MAIL_USERNAME,
-        pass: process.env.MAIL_PASSWORD
-    }
-});
+// const transporter = nodemailer.createTransport({
+//     host: process.env.MAIL_HOST,
+//     port: process.env.MAIL_PORT,
+//     secure: false, // true for 465
+//     auth: {
+//         user: process.env.MAIL_USERNAME,
+//         pass: process.env.MAIL_PASSWORD
+//     }
+// });
+
+// module.exports = transporter;
+
+
+const postmark = require("postmark");
+
+const transporter = new postmark.ServerClient(process.env.POSTMARK_API_KEY);
 
 module.exports = transporter;
