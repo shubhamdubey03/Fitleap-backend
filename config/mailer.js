19f6cb863001dfe266f233a9ddaca99d027097a8
shubhamdubey03 <shubham.dubeyargos@gmail.com>
2026-03-22 10:20:08 +0530

    changes done

--- config/mailer.js
+++ config/mailer.js
@@ -2,10 +2,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
-    // port: process.env.MAIL_PORT,
-    // secure: process.env.MAIL_PORT == 465, // true for 465
-    port: 587,
-    secure: false,
+    port: process.env.MAIL_PORT,
+    secure: false, // true for 465
     auth: {
         user: process.env.MAIL_USERNAME,
         pass: process.env.MAIL_PASSWORD
