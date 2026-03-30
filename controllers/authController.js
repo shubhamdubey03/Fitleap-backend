d7463f49d30158f7f3b1391f564f20de4156840d
shubhamdubey03 <shubham.dubeyargos@gmail.com>
2026-03-30 12:22:09 +0530

    changes done

--- controllers/authController.js
+++ controllers/authController.js
@@ -945,7 +945,7 @@ const login = async (req, res) => {
             if (tokenError) throw tokenError;
 
             try {
-                await sendOtpEmail("aj257453@gmail.com", otp, user.name);
+                await sendOtpEmail(trimmedEmail, otp, user.name);
             } catch (err) {
                 console.error("Failed to send OTP email:", err);
             }
