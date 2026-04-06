94cbc4b4a1f8d011408383cc9179c44523b38250
shubhamdubey03 <shubham.dubeyargos@gmail.com>
2026-04-06 21:30:38 +0530

    changes done

--- routes/userRoute/userRoutes.js
+++ routes/userRoute/userRoutes.js
@@ -4,6 +4,7 @@ const dietController = require('../../controllers/userDiet/userDietController');
 const { protect } = require('../../middleware/authMiddleware');
 
 router.post('/add', protect, dietController.addDiet);
+router.get('/free', protect, dietController.getFreeDiets);       // global free diets - visible to all users
 router.get('/diet/:user_id', protect, dietController.getUserDiet);
 
 module.exports = router;
